ff78f35d22882c1e1dc72643c0f884d48d058393
VR-25 <28943176+VR-25@users.noreply.github.com>
2022-06-25 16:44:23 +0100

    Bump version

--- tarb.sh
+++ tarb.sh
@@ -952,7 +952,7 @@ PASSF=$TMPDIR/.pass
 AUTHOR="VR-25 @ GitHub"
 COPYRIGHT_YEAR=2022
 DESCRIPTION="Backup/restore apps and respective data, SSAIDs, runtime permissions, generic system settings, Magisk modules, and more."
-VERSION="v2022.6.4 202206040"
+VERSION="v2022.6.25 202206250"
 
 [ -z "${LINENO-}" ] || export PS4='$LINENO: '
 mkdir -p ${BKP_DIR##* } $BIN_DIR
